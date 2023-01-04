106b7cbf35ac1195a4e3d5fad7ab499aa0e11884
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-04 08:52:11 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+// import Input, { TextArea } from "../components/Input";
+// import Layout from "../components/Layout";
+
+export default class index extends Component {
+  state = {
+    datas: [
+      {
+        id: 1,
+        title: "Avengers 1",
+        image: "https://pbs.twimg.com/media/FY-BpW9XwAIXIui.jpg",
+      },
+      {
+        id: 2,
+        title: "Avengers 2",
+        image: "https://pbs.twimg.com/media/FY-BpW9XwAIXIui.jpg",
+      },
+      {
+        id: 3,
+        title: "Avengers 3",
+        image: "https://pbs.twimg.com/media/FY-BpW9XwAIXIui.jpg",
+      },
+      {
+        id: 4,
+        title: "Avengers 4",
+        image: "https://pbs.twimg.com/media/FY-BpW9XwAIXIui.jpg",
+      },
+      {
+        id: 5,
+        title: "Avengers 5",
+        image: "https://pbs.twimg.com/media/FY-BpW9XwAIXIui.jpg",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      //   <Layout>
+      <div>Test</div>
+      //   </Layout>
+    );
+  }
+}
