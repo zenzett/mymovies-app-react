7f5ee4d73a74b4958f6116ae5d5dcbe9afdfc467
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-09 06:41:21 +0700

    feat: added GenreType, LanguageType, VideosType, and MovieType to movie util

---
+++ src/utils/types/movie.ts
@@ -0,0 +1,30 @@
+type GenreType = {
+  id?: number;
+  name?: string;
+};
+
+type LanguageType = {
+  english_name?: string;
+};
+
+interface VideosType {
+  id?: string;
+  key?: string;
+  name?: string;
+}
+
+export interface MovieType {
+  id?: number;
+  backdrop_path?: string;
+  poster_path?: string;
+  title?: string;
+  tagline?: string;
+  spoken_languages?: LanguageType[];
+  release_date?: string;
+  runtime?: number;
+  genres?: GenreType[];
+  overview?: string;
+  videos?: {
+    results: VideosType[];
+  };
+}
