01a6917d0ab592372e8cc7119a38ebf7b126b0ec
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-09 06:57:53 +0700

    feat: made navigation to use Wrapper

---
+++ src/utils/navigation.tsx
@@ -0,0 +1,12 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { FC } from "react";
+
+export const withRouter = (Component: any) => {
+  const Wrapper = (props: any) => {
+    const navigate = useNavigate();
+    const params = useParams();
+
+    return <Component {...props} navigate={navigate} params={params} />;
+  };
+  return Wrapper;
+};
