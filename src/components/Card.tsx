733a12dc785c50b57b27b0eb33170b220eb0f42a
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-09 06:48:37 +0700

    feat: implemented routing in card and changed card styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,57 @@
-import React, { Component } from "react";
+import { NavigateFunction } from "react-router-dom";
+import { Component } from "react";
+
+import { FavoriteButton } from "./Button";
+import { withRouter } from "../utils/navigation";
 
 interface CardProps {
+  id: number;
   title: string;
   image: string;
   release_date: string;
+  labelButton: string;
+  onClickFav?: () => void;
+  navigate?: any;
+  params?: any;
 }
 
-export default class Card extends Component<CardProps> {
+class Card extends Component<CardProps> {
+  onClickDetail() {
+    console.log(this.props.id);
+    this.props.navigate(`/detail/${this.props.id}`);
+  }
   render() {
     return (
-      <div className="card card-compact bg-stone-900 shadow-xl glass">
-        <figure>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${this.props.image}`}
-            alt="Image not found."
-          />
+      <div className="card card-compact bg-black shadow-xl glass duration-300 hover:scale-105">
+        <figure onClick={() => this.onClickDetail()}>
+          <button>
+            <img
+              className="duration-300 hover:scale-105 hover:blur-sm"
+              src={`https://image.tmdb.org/t/p/w500${this.props.image}`}
+              alt="Image not found."
+            />
+          </button>
         </figure>
-        <div className="card-body">
-          <span className="flex font-bold text-xs sm:text-base">
-            {this.props.title}
+        <div className="card-body gap-0">
+          <span
+            className="m:0 p:0 flex font-bold text-xs sm:text-base"
+            onClick={() => this.onClickDetail()}
+          >
+            <button>{this.props.title}</button>
           </span>
-          <p className="text-xs sm:text-base">{this.props.release_date}</p>
-          <div className="card-actions flex justify-center mt-10">
-            <button className="btn btn-primary w-full text-xs sm:text-base">
-              Add to favorite
-            </button>
+          <p className="text-xs font-light sm:text-base m:0 p:0">
+            {this.props.release_date}
+          </p>
+          <div className="card-actions flex justify-center mt-5">
+            <FavoriteButton
+              label={this.props.labelButton}
+              onClick={this.props.onClickFav}
+            />
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(Card);
