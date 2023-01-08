e0a6d2f17260a82df0992ff837460b41f2a242ed
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-09 06:56:50 +0700

    feat: import App page for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages";
+import React from "react";
+import axios from "axios";
+
+import App from "./routes";
 import "./styles/index.css";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/movie/";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
