ab6cf4d72205a0e88fcb5714b063da16a3d7bdc2
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-12 01:57:22 +0700

    feat: implemented redux and context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState, useMemo, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import Favorite from "../pages/Favorite";
-import Detail from "../pages/Detail";
 import Homepage from "../pages";
+import Detail from "../pages/Detail";
+import Favorite from "../pages/Favorite";
+
+import { setFavorites } from "../utils/redux/reducers/reducer";
+import { ThemeContext } from "../utils/context";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +25,30 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  const dispatch = useDispatch();
+  const [theme, setTheme] = useState("light");
+  const background = useMemo(() => ({ theme, setTheme }), [theme]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    const getFavMovies = localStorage.getItem("FavMovie");
+    if (getFavMovies) {
+      dispatch(setFavorites(JSON.parse(getFavMovies)));
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={background}>
+      <RouterProvider router={router} />
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
